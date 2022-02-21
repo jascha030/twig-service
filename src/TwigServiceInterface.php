e65c676c9ce0f1f56020fc63abd6693292b26498
Jascha030 <contact@jaschavanaalst.nl>
2022-02-21 11:40:48 +0100

    ✨ TwigServiceInterface

--- src/TwigServiceInterface.php
+++ src/TwigServiceInterface.php
@@ -2,9 +2,24 @@
 
 declare(strict_types=1);
 
+namespace Jascha030\Twig;
+
+use Jascha030\Twig\Templater\TemplaterInterface;
 use Twig\Environment;
 
-interface TwigTemplaterInterface extends TemplaterInterface
+/**
+ * Extends TemplaterInterface, adds method to retrieve Twig Environment.
+ *
+ * @author Jascha030<contact@jasschavanaalst.nl>
+ *
+ * @since  1.0.0
+ */
+interface TwigServiceInterface extends TemplaterInterface
 {
+    /**
+     * Retrieve the used Twig Environment.
+     *
+     * @see https://twig.symfony.com/doc/3.x/api.html#:~:text=Twig%20uses%20a%20central%20object,use%20that%20to%20load%20templates.
+     */
     public function getEnvironment(): Environment;
 }
